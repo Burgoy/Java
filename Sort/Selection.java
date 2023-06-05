55e4433613845b931891a880046233f49585b99d
Vitaly <90931548+Burgoy@users.noreply.github.com>
2023-06-05 13:21:02 +0300

    Методы сортировки массива

--- Sort/Selection.java
+++ Sort/Selection.java
@@ -1,25 +1,42 @@
 package Sort;
 
+
+import java.util.Arrays;
 import java.util.Random;
 
 public class Selection {
-    private int[] array;
+    int[] array;
+    private int min;
+    private int max;
+    Selection(int length, int min, int max){
+        this.min = min;
+        this.max = max;
+        array = new int[length];
+        for(int i = 0; i < array.length; i++){
+            array[i] = new Random().nextInt(min, max);
+        }
+        System.out.println(Arrays.toString(array));
 
-    //Заполняем массив рандомными числами
-    Selection(int size, int min, int max){
-        var rand = new Random();
-        for (int i = 0; i < size; i++){
-            array[i] = rand.nextInt(min, max);
+    }
+   void selectionSort() {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int var = array[minIdx];
+            array[minIdx] = array[i];
+            array[i] = var;
         }
     }
 
-    int min(int[] array, int num){
-        int minIndex = num;
-        int minValue = array[0];
-        for(int i = num + 1; i <array.length; i++){
-            minValue = array[i];
-            minIndex = i;
-        }
-        return minIndex;
+
+    public static void main(String[] args) {
+        Selection selection = new Selection(10, 0, 10);
+        selection.selectionSort();
+        System.out.println(Arrays.toString(selection.array));
     }
 }
